a917861bdf623ae42f5fc0a4a653d3f87521cdd0
Nolan Piland <8548667+pieceOpiland@users.noreply.github.com>
2016-11-26 10:04:37 -0500

    Trying out Heroku.

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const http = require("http");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 
 app.use("/", express.static(path.join(__dirname, "web")));
 
 const srv = http.createServer(app);
 
-srv.listen(8000);
-console.log("Listening on port 8000");
+srv.listen(port);
+console.log("Listening on port %d", port);
