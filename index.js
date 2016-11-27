b5f5a2d02e675ac3f18fd51219cf996287c1ba67
Nolan Piland <8548667+pieceOpiland@users.noreply.github.com>
2016-11-27 17:30:02 -0500

    Trying out node-ws and getting the environment from heroku.

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const WebSocketServer = require("ws").Server;
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-
 app.use("/", express.static(path.join(__dirname, "web")));
 
+app.get("/env", function(req, res){
+  res.json({env: process.env.NODE_ENV});
+});
+
 const srv = http.createServer(app);
+
+const wss = new WebSocketServer({
+  server: srv,
+  path: "/ws"
+});
+
+wss.broadcast = function broadcast(data){
+  wss.clients.forEach(function(client){
+    client.send(data);
+  });
+};
+
+wss.on("connection", function(ws){
+  ws.on("message", function(data){
+    wss.broadcast(data);
+  });
+});
 
 srv.listen(port);
 console.log("Listening on port %d", port);
